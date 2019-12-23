171c76d088f4dbd4526bce67825be039c95a416d
Sao Sai Woon Leng <54533441+saosaiwoonleng@users.noreply.github.com>
2019-12-24 01:26:08 +0800

    Add files via upload

---
+++ Software/SP1/SP1.ino
@@ -0,0 +1,203 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+  // create servo object to control a servo 
+Servo myservo1;                // a maximum of eight servo objects can be created 
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;                // a maximum of eight servo objects can be created 
+Servo myservo6;
+Servo myservo7;
+Servo myservo8;
+Servo myservo9;                // a maximum of eight servo objects can be created 
+Servo myservo10;
+Servo myservo11;
+Servo myservo12;
+int pos = 0;    // variable to store the servo position v
+ 
+void setup() 
+{ 
+  
+ myservo1.attach(30); 
+myservo2.attach(31); 
+myservo3.attach(32); 
+myservo4.attach(33);
+ myservo5.attach(34); 
+myservo6.attach(35); 
+myservo7.attach(36); 
+myservo8.attach(37);
+ myservo9.attach(38); 
+myservo10.attach(39); 
+myservo11.attach(40); 
+myservo12.attach(41);
+
+
+
+
+
+
+ myservo1.write(90);  
+ myservo2.write(90);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60); 
+ delay(1000);// attaches the servo on pin 9 to the servo object 
+} 
+ 
+ 
+void loop() 
+{ 
+  
+ myservo1.write(90);  
+ myservo2.write(90);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+ myservo1.write(150);  
+ myservo2.write(90);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+ 
+  myservo5.write(100);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+myservo1.write(150);  
+ myservo2.write(90);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(150);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(90);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+
+
+
+
+
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(60);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(150);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(60);  
+ myservo4.write(90); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+
+
+
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(60);  
+ myservo4.write(150); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(150);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+myservo1.write(150);  
+ myservo2.write(60);  
+ myservo3.write(60);  
+ myservo4.write(150); 
+ 
+  myservo5.write(60);  
+ myservo6.write(60);  
+ myservo7.write(60);  
+ myservo8.write(60);
+ 
+ myservo9.write(90);  
+ myservo10.write(90);  
+ myservo11.write(90);  
+ myservo12.write(90); 
+delay(2000);
+} 
